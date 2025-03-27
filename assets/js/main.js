9c8151d752fc8c0a9d0cf76f49d71f4dadf9e02b
angel ulises <davila.angel5000@gmail.com>
2025-03-26 21:24:18 -0600

    prueba 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,8 @@ function actualizarHeader(user) {
                         <span>Bienvenido, ${user.displayName || user.email}</span>
                         <button id="logout" class="btn btn-danger">Cerrar Sesión</button>
                     </div>
-            
+                    </div>
+                    </div>
         `;
 
         // Agregar funcionalidad al botón de cierre de sesión
