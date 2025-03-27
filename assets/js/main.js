bfc131c6ef0a7e7ff857b17b7c4d33125fd85e00
angel ulises <davila.angel5000@gmail.com>
2025-03-26 21:17:28 -0600

    prueba login 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,15 +31,25 @@ function actualizarHeader(user) {
 
     if (headerContainer) {
         headerContainer.innerHTML = `
-            <div class="container">
-                <div class="d-flex justify-content-between align-items-center">
-                    <a href="index.html"><img class="logotipo" src="assets/img/LOGO.png"></a>
-                    <div>
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex">
+                <img class="logotipo" src="assets/img/LOGO.png">
+            </a>
+
+            <ul id="cabecera" class="nav col-12 col-lg-auto active me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="index.html" class="nav-link px-2 text-white">Home</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+            </ul>
+            <div>
                         <span>Bienvenido, ${user.displayName || user.email}</span>
                         <button id="logout" class="btn btn-danger">Cerrar Sesión</button>
                     </div>
-                </div>
             </div>
+        </div>
         `;
 
         // Agregar funcionalidad al botón de cierre de sesión
