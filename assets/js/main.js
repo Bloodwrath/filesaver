d35101301bbc1cb16d729daef5f4d575822a601c
angel ulises <davila.angel5000@gmail.com>
2025-03-26 23:53:48 -0600

    PRUEBA LOGIN Y CABECERA DINAMICA

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,37 +25,21 @@ function inicializarAuth() {
     });
 }
 
-// 🔹 Función para actualizar el header con los datos del usuario autenticado
+// 🔹 Dar funcionalidad a boton logout
+document.getElementById("logout").addEventListener("click", function () {
+    signOut(getAuth())
+        .then(() => {
+            console.log("Usuario cerró sesión.");
+            location.reload(); // Recargar la página para mostrar el header original
+        })
+        .catch((error) => console.error("Error al cerrar sesión:", error));
+});
+
 function actualizarHeader(user) {
-    const headerContainer = document.getElementById("header");
-
-    if (headerContainer) {
-        headerContainer.innerHTML = `
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="d-flex">
-                <img class="logotipo" src="assets/img/LOGO.png">
-            </a>
-
-            <ul id="cabecera" class="nav col-12 col-lg-auto active me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="index.html" class="nav-link px-2 text-white">Home</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-            </ul>
-            <div>
-                        <span>Bienvenido, ${user.displayName || user.email}</span>
-                        <button id="logout" class="btn btn-danger">Cerrar Sesión</button>
-                    </div>
-        // Agregar funcionalidad al botón de cierre de sesión
-        document.getElementById("logout").addEventListener("click", function () {
-            signOut(getAuth())
-                .then(() => {
-                    console.log("Usuario cerró sesión.");
-                    location.reload(); // Recargar la página para mostrar el header original
-                })
-                .catch((error) => console.error("Error al cerrar sesión:", error));
-        });
+    const header = document.getElementById("header");
+    if (user) {
+        header.id = "header2"; // Cambiar el id a "header2" si el usuario está autenticado
+    } else {
+        header.id = "header"; // Cambiar el id de vuelta a "header" si no hay usuario
     }
 }
